8f73ecac8e7c2fd7a9d50e92b3049664f490c064
Huang-Huang Bao <i@eh5.me>
2025-02-19 21:16:35 +0800

    chore: update vmlinux.h from linux kernel v6.13

--- src/bpf/kernel/vmlinux.h
+++ src/bpf/kernel/vmlinux.h
@@ -12,32 +12,4 @@ struct bpf_timer {
     __u64 __opaque[2];
 } __attribute__((aligned(8)));
 
-struct bpf_dynptr {
-    __u64 __opaque[2];
-} __attribute__((aligned(8)));
-
-struct bpf_list_head {
-    __u64 __opaque[2];
-} __attribute__((aligned(8)));
-
-struct bpf_list_node {
-    __u64 __opaque[3];
-} __attribute__((aligned(8)));
-
-struct bpf_rb_root {
-    __u64 __opaque[2];
-} __attribute__((aligned(8)));
-
-struct bpf_rb_node {
-    __u64 __opaque[4];
-} __attribute__((aligned(8)));
-
-struct bpf_refcount {
-    __u32 __opaque[1];
-} __attribute__((aligned(4)));
-
-struct bpf_iter_num {
-    __u64 __opaque[1];
-} __attribute__((aligned(8)));
-
 #endif
